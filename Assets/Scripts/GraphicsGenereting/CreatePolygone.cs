d50dc9097b159a5781fb6b5834ef86a99b3e9abe
Ryszard Błażej <sapermysza@gmail.com>
2022-05-19 16:11:50 +0200

    Added calculating polygon corners position

---
+++ Assets/Scripts/GraphicsGenereting/CreatePolygone.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePolygone : MonoBehaviour
+{
+
+    public int lines;
+    public float radius;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            GetPolygon(lines, radius, transform.position);
+        }
+    }
+
+    List<Vector2> GetPolygon(int lines, float radius, Vector2 pivot)
+    {
+        List<Vector2> points = new List<Vector2>();
+        Vector2 circle = new Vector2(0, radius);
+        for (int i = 0; i < lines; i++) 
+        {
+            points.Add(circle + pivot);
+            circle = rotate(circle, Mathf.Deg2Rad * 360 / lines);
+            Debug.DrawLine(circle + pivot, points[points.Count - 1] + pivot, Color.green, 20, false);
+        }
+
+        return points;
+    }
+
+    public static Vector2 rotate(Vector2 v, float delta)
+    {
+        return new Vector2(
+            v.x * Mathf.Cos(delta) - v.y * Mathf.Sin(delta),
+            v.x * Mathf.Sin(delta) + v.y * Mathf.Cos(delta)
+        );
+    }
+
+}
